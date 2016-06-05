bb8b9bd95025bc86b75e68f7db60ee3b8db203c8
Brian Neary <bri.neary@gmail.com>
2016-06-05 14:15:23 -0400

    Added submit to console, text field and email validation, tweaks
    Send Mail now created and object ready to submit.  Validation for fields
    in place.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,6 +18,11 @@ $(document).ready(function(){
 	*/
 	$(".new-email").hide();
 
+	function validateEmail(email) {
+	    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    return re.test(email);
+	}
+
 	function drawEmailBtns(addresses, btnStyles){
 		$(".email-buttons .btn").remove();
 		$.each(addresses, function(index, tag){
@@ -37,8 +42,42 @@ $(document).ready(function(){
 		$(".new-email").toggle();
 	});
 	$(".append-email").click(function(){
-		addresses.push($("#new-email").val());
-		drawEmailBtns(addresses,btnStyles);
-		$(".new-email").toggle();
+		if(validateEmail($("#new-email").val()) == true ){
+			addresses.push($("#new-email").val());
+			drawEmailBtns(addresses,btnStyles);
+			$(".new-email").toggle();
+		} else {
+			alert("Please enter a valid email address");
+		}
+	});
+
+	$(".email-buttons .btn").click(function(){
+		var emailToRemove = $(this).val();
+		console.log("removing: " + emailToRemove);
+
+		for (var i=addresses.length - 1; i >= 0; i--) {
+		    if (addresses[i] === emailToRemove) {
+		        addresses.splice(i, 1);
+		        // break;       //<-- Uncomment  if only the first term has to be removed
+		    }
+		}
+		console.log("addresses: " + typeof addresses);
+		drawEmailBtns(addresses, btnStyles);
+	});
+
+	$("#send-mail").click(function(){
+		if($("#subject").val() == null || $("#subject").val() == "" || $("#message").val() == null || $("#message").val() == ""){
+			alert("Please fill out both the Subject and Message fields");
+			return;
+		}
+		var dataToSubmit = {
+			"addresses": addresses,
+			"subject": $("#subject").val(),
+			"message": $("#message").val(),
+			"savecopy": $("#savecopy").is(':checked')
+		};
+
+		// There's no back-end to work with, so simply output the collected data to the console
+		console.dir(dataToSubmit);
 	});
 });
